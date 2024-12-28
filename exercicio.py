68c880abdd93b9e85c0802dfcc56e5ef0d4ce5de
6iove <jcostacaramori@gmail.com>
2024-12-27 22:41:31 -0300

    função gerar numero de matricula

--- exercicio.py
+++ exercicio.py
@@ -1,3 +1,6 @@
+import random 
+import string
+
 alunos_cadastrados = []
 
 def cadastro_de_alunos():
@@ -8,16 +11,26 @@ def cadastro_de_alunos():
     telefone = input("Número de telefone: ")
     email = input("E-mail: ")
     
+    gerar_numero_de_matricula()
+    
     aluno = {
         "Nome": nome, 
         "Data de nascimento": data_de_nascimento, 
         "Gênero sexual": genero_sexual, 
         "Endereço": endereço, 
         "Telefone": telefone, 
-        "E-mail": email
+        "E-mail": email,
+        "Matrícula": gerar_numero_de_matricula() 
     }
     
     alunos_cadastrados.append(aluno)
     return alunos_cadastrados
 
-print(cadastro_de_alunos())
+
+def gerar_numero_de_matricula():
+    numero_matricula = ''.join(random.choices(string.digits, k = 6))
+    letra_matricula = random.choice(string.ascii_uppercase)
+    return f"{numero_matricula}-{letra_matricula}"
+  
+print(cadastro_de_alunos()) 
+
