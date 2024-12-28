9d60571123cdca0f36362680e0a3d2e9873ec660
6iove <jcostacaramori@gmail.com>
2024-12-27 22:26:22 -0300

    função cadastro de aluno

--- exercicio.py
+++ exercicio.py
@@ -1,2 +1,23 @@
-print("Teste primeiro commit")
-print("Teste primeiro commit")
+alunos_cadastrados = []
+
+def cadastro_de_alunos():
+    nome = input("Nome do aluno: ")
+    data_de_nascimento = input("Data de nascimento: ")
+    genero_sexual = input("Gênero sexual: ")
+    endereço = input("Endereço completo: ")
+    telefone = input("Número de telefone: ")
+    email = input("E-mail: ")
+    
+    aluno = {
+        "Nome": nome, 
+        "Data de nascimento": data_de_nascimento, 
+        "Gênero sexual": genero_sexual, 
+        "Endereço": endereço, 
+        "Telefone": telefone, 
+        "E-mail": email
+    }
+    
+    alunos_cadastrados.append(aluno)
+    return alunos_cadastrados
+
+print(cadastro_de_alunos())
