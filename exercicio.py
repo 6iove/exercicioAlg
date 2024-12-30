e452f89bde37e4dceca625e2254212a64f179faa
6iove <jcostacaramori@gmail.com>
2024-12-30 10:06:27 -0300

    função cadastro de turmas e gerar código de turmas

--- exercicio.py
+++ exercicio.py
@@ -72,6 +72,8 @@ print(cadastro_de_professores())
 disciplinas_cadastradas = []
 
 def cadastro_de_disciplinas():
+    print("*** CADASTRAR DISCIPLINAS ***")
+    
     nome_da_disciplina = input("Disciplina: ")
     carga_horaria = int(input("Carga horária: "))
     professor_da_disciplina = input("Discente: ")
@@ -92,3 +94,31 @@ def gerar_codigo_disciplina():
     return f"{letra_codigo_disciplina}{numero_codigo_disciplina}" 
 
 print(cadastro_de_disciplinas())
+
+turmas_cadastradas = []
+
+def cadastro_de_turmas():
+    print("*** CADASTRAR TURMAS ***")
+    
+    nome_turma = input("Nome da turma: ")
+    disciplinas_da_turma = input("Disciplinas lecionadas à turma: ")
+    professor_da_turma = input("Discentes da turma: ")
+    alunos_da_turma = input("Alunos matrículados na turma: ")
+    
+    turma = {
+        "Nome da turma": nome_turma,
+        "Disciplinas": disciplinas_da_turma,
+        "Professores": professor_da_turma,
+        "Alunos": alunos_da_turma,
+        "Código": gerar_código_turma()
+    }
+    
+    turmas_cadastradas.append(turma)
+    return turmas_cadastradas
+
+def gerar_código_turma():
+    numero_codigo_turma = ''.join(random.choices(string.digits, k = 4))
+    letra_codigo_turma = random.choice(string.ascii_uppercase)
+    return f"{letra_codigo_turma}{numero_codigo_turma}" 
+
+print(cadastro_de_turmas())
