2e1f5bd51c7d2f3481aa71942592474c7a290d45
6iove <jcostacaramori@gmail.com>
2025-01-03 09:58:29 -0300

    função consultar alunos em turmas

--- exercicio.py
+++ exercicio.py
@@ -43,7 +43,7 @@ def gerar_numero_de_matricula():
 def matricular_aluno_em_turma():
     print("\n*** MATRICULAR ALUNO EM TURMA ***")
     
-    matricula_do_aluno = input("Digite a matrícula do aluno: ") # mais de um aluno pode ter o mesmo nome completo
+    matricula_do_aluno = input("Digite a matrícula do aluno: ").strip() # mais de um aluno pode ter o mesmo nome completo
     aluno_encontrado = None # cria variável vazia
     
     # percorre array alunos_cadastrados 
@@ -56,7 +56,7 @@ def matricular_aluno_em_turma():
         
     if aluno_encontrado:
         print(f"Aluno: {aluno_encontrado['Nome']}")
-        nome_da_turma = input("Em qual turma o aluno será matrículado? ")
+        nome_da_turma = input("Em qual turma o aluno será matrículado? ").strip()
         turma_encontrada = None
         
         for turma in turmas_cadastradas:
@@ -117,7 +117,7 @@ def gerar_matricula_do_professor():
 def alocar_professor_em_disciplina():
     print("\n*** ALOCAR PROFESSOR EM DISCIPLINA ***")
     
-    matricula_do_professor = input("Digite a matrícula do professor: ") # mais de um professor pode ter o mesmo nome completo
+    matricula_do_professor = input("Digite a matrícula do professor: ").strip() # mais de um professor pode ter o mesmo nome completo
     professor_encontrado = None # cria variável vazia
     
     # percorre array professores_cadastrados 
@@ -134,7 +134,7 @@ def alocar_professor_em_disciplina():
     
     print(f"Professor registrado: {professor_encontrado['Nome']}")
     
-    nome_nova_disciplina = input("Digite o nome da disciplina em que o professor será alocado: ")
+    nome_nova_disciplina = input("Digite o nome da disciplina em que o professor será alocado: ").strip()
 
     if nome_nova_disciplina not in professor_encontrado['Disciplinas']:
         professor_encontrado['Disciplinas'].append(nome_nova_disciplina)
@@ -192,7 +192,7 @@ def gerar_codigo_disciplina():
 def alocar_disciplina_em_turmas():
     print("\n*** ALOCAR PROFESSOR EM DISCIPLINA ***")
    
-    nome_disciplina = input("Digite o nome da disciplina: ")
+    nome_disciplina = input("Digite o nome da disciplina: ").strip()
     disciplina_encontrada = None # cria variável vazia
    
     for disciplina in disciplinas_cadastradas:
@@ -204,7 +204,7 @@ def alocar_disciplina_em_turmas():
         print("\nNenhuma disciplina encontrada com esse nome. Tente novamente.")
         return
    
-    nome_turma = input("Digite o nome da turma: ")
+    nome_turma = input("Digite o nome da turma: ").strip()
     turma_encontrada = None
    
     for turma in turmas_cadastradas:
@@ -250,6 +250,25 @@ def cadastro_de_turmas():
         
     return turmas_cadastradas
 
+
+def consultar_alunos_em_turmas():
+    print("\n*** CONSULTAR ALUNOS EM TURMAS ***")
+    
+    nome_turma = input("Digite o nome da turma que deseja consultar: ").strip()
+    turma_encontrada = None
+    
+    for turma in turmas_cadastradas:
+        if turma['Nome da turma'].lower() == nome_turma.lower():
+            turma_encontrada = turma
+            break
+    
+    if turma_encontrada['Alunos']:
+        print(f"\nAlunos matrículados na turma {nome_turma}: ")
+        for aluno in turma_encontrada['Alunos']:
+                print (f"{aluno['Nome']}, Matrícula: {aluno['Matrícula']}")
+    else:
+        print(f"\nParece que não há alunos matriculados na turma {nome_turma} ainda.")
+
 def gerar_código_turma():
     numero_codigo_turma = ''.join(random.choices(string.digits, k = 4))
     letra_codigo_turma = random.choice(string.ascii_uppercase)
@@ -269,6 +288,7 @@ def pagina_inicial():
         print("5. Opções de filtragem")
         print("6. Matrícula de alunos em turmas")
         print("7. Opções de alocação")
+        print("8. Opções de consulta")
         print("Sair")
 
         opcao_cadastro = input("Escolha uma opção (1-6): ")
@@ -302,6 +322,11 @@ def pagina_inicial():
                 alocar_professor_em_disciplina()
             if opcao_alocacao == "2":
                 alocar_disciplina_em_turmas()
+        elif opcao_cadastro == "8":
+            print("1. Consulta de alunos matriculados em turmas")
+            opcao_consulta = input("Escolha uma opção de consulta (1-3): ")
+            if opcao_consulta == "1":
+                consultar_alunos_em_turmas()
         else:
             print("Opção inválida. Digite 1, 2, 3, 4, ou 5.")
             
